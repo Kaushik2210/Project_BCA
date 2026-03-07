b128e378370d9af10428bed9918378d470f6ae2d
judechowri2005-tech <judechowri2005@gmail.com>
2026-03-07 11:21:23 +0530

    Update contact information on ContactPage

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -101,7 +101,7 @@ const ContactPage = () => {
                                     </div>
                                     <div>
                                         <h3 className="text-xl font-serif text-[#f0e6d2] mb-1">Our Location</h3>
-                                        <p className="text-[#f0e6d2]/70">123 Faith Avenue<br/>Graceville, ST 12345</p>
+                                        <p className="text-[#f0e6d2]/70">#839 HSR Layout<br/>Bengaluru, Karnataka</p>
                                     </div>
                                 </div>
                                 
@@ -111,7 +111,7 @@ const ContactPage = () => {
                                     </div>
                                     <div>
                                         <h3 className="text-xl font-serif text-[#f0e6d2] mb-1">Phone Number</h3>
-                                        <p className="text-[#f0e6d2]/70">(555) 123-4567</p>
+                                        <p className="text-[#f0e6d2]/70">90084 69800</p>
                                     </div>
                                 </div>
 
@@ -121,7 +121,7 @@ const ContactPage = () => {
                                     </div>
                                     <div>
                                         <h3 className="text-xl font-serif text-[#f0e6d2] mb-1">Email Address</h3>
-                                        <p className="text-[#f0e6d2]/70">info@resurrectionbaptist.org</p>
+                                        <p className="text-[#f0e6d2]/70">jonahchowri95@gmail.com</p>
                                     </div>
                                 </div>
                             </div>
