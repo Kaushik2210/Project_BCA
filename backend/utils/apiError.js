f23e63a344886a61196362ae5f0958d8d41557cd
renuthomas <96842019+renuthomas@users.noreply.github.com>
2026-03-08 00:16:24 +0530

    feat: add toJSON method to return JSON object

--- backend/utils/apiError.js
+++ backend/utils/apiError.js
@@ -1,16 +1,27 @@
 class ApiError extends Error{
     constructor(statusCode,message="Error occured",errors=[],stack=""){
         super(message);
+        this.message=message;
+
         this.statusCode=statusCode;
         this.data=null;
-        this.message=message;
         this.success=false;
         this.errors=errors;
         if(stack){
             this.stack=stack;
         }else{
-            this.stack=Error.captureStackTrace();
+            Error.captureStackTrace(this,ApiError);
         }
+    }
+
+    toJSON() {
+        return {
+            statusCode: this.statusCode,
+            success: this.success,
+            message: this.message,
+            data: this.data,
+            errors: this.errors,
+        };
     }
 }
 
--- backend/utils/apiResponse.js
+++ backend/utils/apiResponse.js
@@ -5,6 +5,16 @@ class ApiResponse{
         this.message=message;
         this.success=statusCode<400;
     }
+
+    //toJSON method to convert the ApiResponse instance to a JSON object since Error class fields are not enumerable by default
+    toJSON() {
+        return {
+            statusCode: this.statusCode,
+            success: this.success,
+            message: this.message,
+            data: this.data,
+        };
+    }
 }
 
 export {ApiResponse}
