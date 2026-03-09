14883de86badb5f6863a52c7e4e53d3e6bf1577f
renuthomas <96842019+renuthomas@users.noreply.github.com>
2026-03-09 15:16:16 +0530

    add catch statement for failed DB connection

--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,7 @@ connecttoDB().then(()=>{
     app.listen(PORT,()=>{
          console.log(`The server is running on localhost:${PORT}`)
     })
-})
+}).catch((error)=>{
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+});
